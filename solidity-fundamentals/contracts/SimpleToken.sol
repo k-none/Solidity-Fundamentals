da40c5a863bed93329c827c5cf36bc4afc8b93ab
k-none <kenanjadav@gmail.com>
2025-08-23 18:23:25 +0530

    Update SimpleToken.sol

--- solidity-fundamentals/contracts/SimpleToken.sol
+++ solidity-fundamentals/contracts/SimpleToken.sol
@@ -8,20 +8,58 @@ contract SimpleToken {
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor(uint256 _initialSupply) {
-        totalSupply = _initialSupply * 10**uint256(decimals);
+        totalSupply = _initialSupply * 10 ** uint256(decimals);
         balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value, "Not enough tokens");
-        unchecked { balanceOf[msg.sender] -= _value; }
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0), "Cannot transfer to zero address");
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+
+        _beforeTokenTransfer(msg.sender, _to, _value);
+
+        unchecked {
+            balanceOf[msg.sender] -= _value;
+        }
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        require(_spender != address(0), "Cannot approve zero address");
+
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_from != address(0), "From address is zero");
+        require(_to != address(0), "To address is zero");
+        require(balanceOf[_from] >= _value, "Insufficient balance");
+        require(allowance[_from][msg.sender] >= _value, "Allowance exceeded");
+
+        _beforeTokenTransfer(_from, _to, _value);
+
+        unchecked {
+            balanceOf[_from] -= _value;
+            allowance[_from][msg.sender] -= _value;
+        }
+        balanceOf[_to] += _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    // Optional hook for extensibility (like adding fees, freezing, etc.)
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual {}
 }
