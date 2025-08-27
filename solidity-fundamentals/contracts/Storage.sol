e99ccc1b1ea802ac58b454abca9e2613c49d14b1
k-none <kenanjadav@gmail.com>
2025-08-27 11:22:42 +0530

    Update Storage.sol

--- solidity-fundamentals/contracts/Storage.sol
+++ solidity-fundamentals/contracts/Storage.sol
@@ -3,9 +3,22 @@ pragma solidity ^0.8.24;
 
 contract Storage {
     uint256 private number;
+    address public owner;
 
-    function store(uint256 num) public {
+    event NumberStored(uint256 newNumber, address indexed sender);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    function store(uint256 num) public onlyOwner {
         number = num;
+        emit NumberStored(num, msg.sender);
     }
 
     function retrieve() public view returns (uint256) {
